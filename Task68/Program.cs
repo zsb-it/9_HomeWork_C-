4134ed9cecb12203b58f256c238c6a7a7a88df9e
Dmitry Kireev <zzsb@ya.ru>
2022-11-16 19:47:52 +0300

    Task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -5,3 +5,18 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+
+Console.WriteLine("Введите целое число m:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число n:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int FunctionAkerman(int m, int n)
+{
+    if(m==0) return n+1;
+    else if (m>0 && n==0) return FunctionAkerman(m-1, 1);
+    else return FunctionAkerman(m-1, FunctionAkerman(m, n-1));
+}
+
+int functionAkerman = FunctionAkerman(numberM, numberN);
+Console.WriteLine($"A({numberM},{numberN}) = {functionAkerman}");
