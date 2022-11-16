cac7eb94662265ca337ea8f03b851938d8e863b7
Dmitry Kireev <zzsb@ya.ru>
2022-11-16 19:28:22 +0300

    Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -5,3 +5,16 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine("Введите целое число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int numM, int numN)
+{
+    if (numM == numN) return numM;
+    else if (numM < numN) return numM + SumNumbers(numM + 1, numN);
+    else return numN + SumNumbers(numM, numN + 1);
+}
+int res = SumNumbers(numberM, numberN);
+Console.WriteLine(res);
